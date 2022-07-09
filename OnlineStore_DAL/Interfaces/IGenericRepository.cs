6ddcb7b24d332c8fcba404ca360639684b994912
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2022-07-09 21:27:04 +0300

    ~ Totally redisigned repository interfaces. ~
    - Removed all concrete repository interfaces for all the models.
    - Made one generic interface for all repositories.

--- OnlineStore_DAL/Interfaces/IGenericRepository.cs
+++ OnlineStore_DAL/Interfaces/IGenericRepository.cs
@@ -1,13 +1,18 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OnlineStore_DAL.Interfaces
 {
     public interface IGenericRepository<T> where T : class
     {
+        IEnumerable<T> GetAllAsync();
+
+        Task<T> GetAsync(int id);
+
         Task AddAsync(T entity);
 
         Task UpdateAsync(T entity);
 
-        Task DeleteByIdAsync(int id);
+        Task DeleteAsync(int id);
     }
 }
