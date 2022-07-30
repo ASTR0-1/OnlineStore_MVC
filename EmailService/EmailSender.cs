d59d69f12178cc94121dc599c8990db00aadee92
Danil Dukhovnyi <duhovny.daniil@gmail.com>
2022-07-30 22:32:00 +0300

    ~ Added EmailService ~

---
+++ EmailService/EmailSender.cs
@@ -0,0 +1,52 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfiguration;
+
+        public EmailSender(EmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+
+        public async Task SendEmailAsync(Message message)
+        {
+            var mailMessage = CreateEmailMessage(message);
+
+            try
+            {
+                using (var smtpClient = new SmtpClient())
+                {
+                    await smtpClient.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+                    smtpClient.AuthenticationMechanisms.Remove("XOAUTH2");
+                    await smtpClient.AuthenticateAsync(_emailConfiguration.UserName, _emailConfiguration.Password);
+
+                    await smtpClient.SendAsync(mailMessage);
+
+                    await smtpClient.DisconnectAsync(true);
+                }
+            }
+            catch
+            {
+                // Add txt logger (separated from db logger)
+                throw;
+            }
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress(_emailConfiguration.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+    }
+}
---
+++ EmailService/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(Message message);
+    }
+}
